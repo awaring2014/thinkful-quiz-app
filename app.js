95e3576017b2bfdb57f3d3aa58a22e9ae66d9663
chasingSublimity <blakemsager@gmail.com>
2016-12-07 19:44:38 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// state variable
+var state = {
+	// holds quiz question objects. Question objects contain a question (string) and answers (string)
+	quiz: []
+};
+// functions to modify state
+	
+// functions to render state
+
+// event listeners to call state-modifying and state-rendering functions
+
+$(document).ready(function() {
+
+});
