1b1e2049d0cd0586d7ef10ea33de6c41defce65d
chasingSublimity <blakemsager@gmail.com>
2016-12-08 09:07:40 -0600

    add buildQuestion and renderQuestionPage functions

--- app.js
+++ app.js
@@ -52,28 +52,66 @@ var questions = [
 	}
 ];
 
+// question-containg object. From here, question objects will be pushed one at a time to state.quiz
+var questionObject = [];
+
 // state variable
 var state = {
 	// holds quiz question objects. Question objects contain a question (string) and answers (string)
-	quiz: []
+	quiz: {}
 };
 
 // functions to modify state
 
-	// function to append random question objects to quiz array
-	function createQuiz(state) {
+	// function to populate questionObject with 5 random questions
+	function createQuizObject(array1, array2) {
 		for (i=0; i < 5; i++) {
-			state.quiz.push(questions[Math.floor(Math.random() * 6) + 1]);
+			array2.push(array1[Math.floor(Math.random() * 6) + 1]);
 		}
-		console.log(state.quiz);
 	}
 
+	// function to push single question object into state.quiz
+	function pushQuestion(state, object, questionIndex) {
+		state.quiz.question = object[questionIndex];
+	}
+
+	// function to cycle through questions
+
+	// function to reset state
 	function resetState(state) {
 		state.quiz = [];
-
 	}
 
 // functions to render state
+
+	// function to build question page in html
+	function buildQuestion(state) {
+		var questionText = 'Of the choices, for which idea is ' + state.quiz.question.philosopher + ' best known?';
+		return $(
+				'<h3>This box will hold the quiz progress</h3>' +
+				'<p class="content">' + questionText + '</p>' +
+				'<div>' +
+		      '<form action="">' +
+		        '<input type="radio" name="quiz-answer" id="answer1" val="answer1" checked />' +
+		        '<label for="answer1">' + state.quiz.question.answer + '</label>' +
+		        '<br>' +
+		        '<input type="radio" name="quiz-answer" id="answer2" val="answer2" />' +
+		        '<label for="answer2">' + state.quiz.question.incorrectAnswers[0] + '</label>' +
+		        '<br>' +
+		        '<input type="radio" name="quiz-answer" id="answer3" val="answer3" />' +
+		        '<label for="answer2">' + state.quiz.question.incorrectAnswers[1] + '</label>' +
+		        '<br>' +
+		        '<input type="radio" name="quiz-answer" id="answer4" val="answer4" />' +
+		        '<label for="answer2">' + state.quiz.question.incorrectAnswers[2] + '</label>' +
+		      '</form>' +
+		    '</div>' +
+		    '<button>Submit</button>'
+			);
+	}
+
+	function renderQuestionPage(element, state) {
+		element.html(buildQuestion(state));
+	}
 
 // event listeners to call state-modifying and state-rendering functions
 
