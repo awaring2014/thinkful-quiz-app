5a47cc364207d4a27aa92ee34d2df3fa9183c8e0
chasingSublimity <blakemsager@gmail.com>
2016-12-07 19:55:34 -0600

    add quiz question objects

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+// array of question objects
+var questions = [
+	{
+		philosopher: 'Plato',
+		answer: 'Forms',
+		incorrectAnswers: ['Substance Dualism', '4 causes', 'Monads']
+	},
+	{
+		philosopher: 'Aristotle',
+		answer: '4 Causes',
+		incorrectAnswers: ['Substance Dualism', 'Forms', 'Divine Atemporalism']
+	},
+	{
+		philosopher: 'Augustine',
+		answer: 'Divine Atemporalism',
+		incorrectAnswers: ['Substance Dualism', '4 causes', 'Necessary Connection']
+	},
+	{
+		philosopher: 'Plotinus',
+		answer: 'Neoplatonism',
+		incorrectAnswers: ['Atemporalism', '4 causes', 'Monads']
+	},
+	{
+		philosopher: 'Hume',
+		answer: 'Necessary Connection',
+		incorrectAnswers: ['Substance Dualism', 'Forms', 'Neoplatonism']
+	},
+	{
+		philosopher: 'Kant',
+		answer: 'Synthetic Aprori Knowledge',
+		incorrectAnswers: ['Forms', 'Scientific Determinism', 'Monads']
+	},
+	{
+		philosopher: 'Descartes',
+		answer: 'Substance Dualism',
+		incorrectAnswers: ['Forms', '4 causes', 'Necessary Connection']
+	},
+	{
+		philosopher: 'David Lewis',
+		answer: 'Modal Realism',
+		incorrectAnswers: ['Substance Dualism', '4 causes', 'Divine Atemporalism']
+	},
+	{
+		philosopher: 'Saul Kripke',
+		answer: 'Necessity of Identity',
+		incorrectAnswers: ['Substance Dualism', '4 causes', 'Monads']
+	},
+	{
+		philosopher: 'Edmund Burke',
+		answer: 'Beauty and the Sublime',
+		incorrectAnswers: ['Necessary Connection', '4 causes', 'Monads']
+	},
+];
+
 // state variable
 var state = {
 	// holds quiz question objects. Question objects contain a question (string) and answers (string)
