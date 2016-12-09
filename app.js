020d0db07983d31593802590ce72d5ef85840b1c
chasingSublimity <blakemsager@gmail.com>
2016-12-09 14:45:14 -0600

    add buildFeedback function

--- app.js
+++ app.js
@@ -52,8 +52,20 @@ var questions = [
 	}
 ];
 
-// question-containg object. From here, question objects will be pushed one at a time to state.quiz
-var questionObject = [];
+// object containing feedback objects. From here, feedback objects will be pushed one at a time to state.quiz
+var feedbackObject = {
+	correct: {
+		h3: "You are correct!",
+		p: "Click continue to answer another question!"
+	},
+	incorrect: {
+		h3: "You are incorrect!",
+		p: "Click continue to answer another question!"
+	}
+};
+
+// array containing question objects. From here, question objects will be pushed one at a time to state.quiz
+var questionObjectArray = [];
 
 // state variable
 var state = {
@@ -63,7 +75,7 @@ var state = {
 
 // functions to modify state
 
-	// function to populate questionObject with 5 random questions
+	// function to populate questionObjectArray with 5 random questions
 	function createQuizObject(array1, array2) {
 		for (i=0; i < 5; i++) {
 			array2.push(array1[Math.floor(Math.random() * 6) + 1]);
@@ -73,6 +85,27 @@ var state = {
 	// function to push single question object into state.quiz
 	function pushQuestion(state, object, questionIndex) {
 		state.quiz.question = object[questionIndex];
+	}
+
+	// function to initialize Question Progress object w/i state.quiz
+	function initQuestionProgressCounter(state) {
+		state.quiz.progress = 1;
+	}
+
+	// function to increment Question Progress object
+	function incrementQuestionProgressCounter(state) {
+		state.quiz.progress++;
+	}
+
+	// function to push feedback object to state based on answer chosen
+	function pushFeedback(state, object) {
+		var jsFormVal = $("input[type='radio'][name='quiz-answer']:checked").val();
+		console.log(jsFormVal);
+		if (jsFormVal === 'answer1') {
+			state.quiz.feedback = object.correct;
+		} else {
+			state.quiz.feedback = object.incorrect;
+		}
 	}
 
 	// function to cycle through questions
@@ -91,26 +124,39 @@ var state = {
 				'<h3>This box will hold the quiz progress</h3>' +
 				'<p class="content">' + questionText + '</p>' +
 				'<div>' +
-		      '<form action="">' +
-		        '<input type="radio" name="quiz-answer" id="answer1" val="answer1" checked />' +
+		      '<form id="js-quiz">' +
+		        '<input type="radio" name="quiz-answer" id="answer1" value="answer1" checked />' +
 		        '<label for="answer1">' + state.quiz.question.answer + '</label>' +
 		        '<br>' +
-		        '<input type="radio" name="quiz-answer" id="answer2" val="answer2" />' +
+		        '<input type="radio" name="quiz-answer" id="answer2" value="answer2" />' +
 		        '<label for="answer2">' + state.quiz.question.incorrectAnswers[0] + '</label>' +
 		        '<br>' +
-		        '<input type="radio" name="quiz-answer" id="answer3" val="answer3" />' +
+		        '<input type="radio" name="quiz-answer" id="answer3" value="answer3" />' +
 		        '<label for="answer2">' + state.quiz.question.incorrectAnswers[1] + '</label>' +
 		        '<br>' +
-		        '<input type="radio" name="quiz-answer" id="answer4" val="answer4" />' +
+		        '<input type="radio" name="quiz-answer" id="answer4" value="answer4" />' +
 		        '<label for="answer2">' + state.quiz.question.incorrectAnswers[2] + '</label>' +
+		        '<br>' +
+		        '<button type="submit">Submit</button>' +
 		      '</form>' +
-		    '</div>' +
-		    '<button>Submit</button>'
+		    '</div>'
 			);
+	}
+
+	function buildFeedbackPage (state) {
+		return $(
+			'<h3>' + state.quiz.feedback.h3 + '</h3>' +
+  		'<p class="content">' + state.quiz.feedback.p + '</p>' +
+  		'<button>Continue</button>'
+		);
 	}
 
 	function renderQuestionPage(element, state) {
 		element.html(buildQuestion(state));
+	}
+
+	function renderFeedbackPage(element, state) {
+		element.html(buildFeedbackPage(state));
 	}
 
 // event listeners to call state-modifying and state-rendering functions
@@ -118,10 +164,26 @@ var state = {
 $(document).ready(function() {
 
 	// begin quiz
+	$('.begin').click(function (event) {
+		event.preventDefault();
+		createQuizObject(questions, questionObjectArray);
+		pushQuestion(state, questionObjectArray, 0);
+		renderQuestionPage($('main'), state);
+		initQuestionProgressCounter(state);
+	});
 
 	// submit answer
+	$(document).on('submit', '#js-quiz', function(event) {
+		event.preventDefault();
+		console.log('submitted');
+		pushFeedback(state, feedbackObject);
+		renderFeedbackPage($('main'), state);
+	});
 
 	// continue quiz
+	// $('.continue').click(function(event) {
+	// 	incrementQuestionProgressCounter(state);
+	// });
 
 	// restart quiz
 
